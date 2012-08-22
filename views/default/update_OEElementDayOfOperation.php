3a147412ce10d0cdb2d477cc3f0583577df9f668
Mark Wadham <mark.wadham@openeyes.org.uk>
2012-08-22 13:51:47 +0100

    [OE-1936] updated to use the new datatype element div properties from examination.

--- views/default/update_OEElementDayOfOperation.php
+++ views/default/update_OEElementDayOfOperation.php
@@ -16,11 +16,14 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
  ?>
-<div class="<?php echo $element->elementType->class_name?>">
+<div class="element <?php echo $element->elementType->class_name?>"
+	data-element-type-id="<?php echo $element->elementType->id ?>"
+	data-element-type-class="<?php echo $element->elementType->class_name ?>"
+	data-element-type-name="<?php echo $element->elementType->name ?>"
+	data-element-display-order="<?php echo $element->elementType->display_order ?>">
 	<h4 class="elementTypeName"><?php  echo $element->elementType->name; ?></h4>
-
-			<?php echo $form->radioBoolean($element, 'medical_history'); ?>
-			<?php echo $form->textField($element, 'inr_level', array('size' => '10')); ?>
-			<?php echo $form->checkBox($element, 'preop_checklist_completed'); ?>
-			<?php echo $form->checkBox($element, 'cjd_checklist_completed'); ?>
-	</div>
+	<?php echo $form->radioBoolean($element, 'medical_history'); ?>
+	<?php echo $form->textField($element, 'inr_level', array('size' => '10')); ?>
+	<?php echo $form->checkBox($element, 'preop_checklist_completed'); ?>
+	<?php echo $form->checkBox($element, 'cjd_checklist_completed'); ?>
+</div>
