fed5aaa0e4132f0a08f9f639d7ac2087b92e7964
Adam Dyer <progen91@gmail.com>
2013-08-19 14:46:07 +0100

    [OE-3107] added foreign key setup to migration

--- migrations/m130814_073532_create_cataract_nurses.php
+++ migrations/m130814_073532_create_cataract_nurses.php
@@ -4,6 +4,7 @@ class m130814_073532_create_cataract_nurses extends CDbMigration
 {
 	public function up()
 	{
+
         // create the table for this element type: et_modulename_elementtypename
         $this->createTable('ophcioperationdayassessment_cataract_nurses', array(
             'id' => 'int(10) unsigned NOT NULL AUTO_INCREMENT',
@@ -15,11 +16,20 @@ class m130814_073532_create_cataract_nurses extends CDbMigration
             'PRIMARY KEY (`id`)',
         ), 'ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin');
 
+
+        $this->addColumn('et_ophcioperationdayassessment_anaesthetic','completed_cataract_nurse_id','int(10) unsigned NOT NULL');
+        $this->createIndex('et_ophcioperationdayassessment_ana_catnurse_id_fk','et_ophcioperationdayassessment_anaesthetic','completed_cataract_nurse_id');
+        $this->addForeignKey('et_ophcioperationdayassessment_ana_catnurse_id_fk','et_ophcioperationdayassessment_anaesthetic','completed_cataract_nurse_id','ophcioperationdayassessment_cataract_nurses','id');
+
+
 	}
 
 	public function down()
 	{
-		$this->dropTable('ophcioperationdayassessment_cataract_nurses');
+        $this->dropForeignKey('et_ophcioperationdayassessment_ana_catnurse_id_fk','et_ophcioperationdayassessment_anaesthetic');
+        $this->dropIndex('et_ophcioperationdayassessment_ana_catnurse_id_fk','et_ophcioperationdayassessment_anaesthetic');
+        $this->dropColumn('et_ophcioperationdayassessment_anaesthetic','completed_cataract_nurse_id');
+        $this->dropTable('ophcioperationdayassessment_cataract_nurses');
 	}
 
 
