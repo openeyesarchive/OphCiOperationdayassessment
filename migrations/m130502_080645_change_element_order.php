67171add4561d04674c33254880c89b4389c8baa
Mark <m@rkw.me>
2014-01-14 08:48:46 +0000

    removed references to models

--- migrations/m130502_080645_change_element_order.php
+++ migrations/m130502_080645_change_element_order.php
@@ -4,15 +4,15 @@ class m130502_080645_change_element_order extends CDbMigration
 {
 	public function up()
 	{
-		$event_type = EventType::model()->find('class_name=?',array('OphCiOperationdayassessment'));
-		$this->update('element_type',array('display_order'=>10),"event_type_id = $event_type->id and class_name = 'Element_OphCiOperationdayassessment_Anaesthetic'");
-		$this->update('element_type',array('display_order'=>20),"event_type_id = $event_type->id and class_name = 'OEElementDayOfOperation'");
+		$event_type = $this->dbConnection->createCommand()->select("*")->from("event_type")->where("class_name = :class_name",array(":class_name" => "OphCiOperationdayassessment"))->queryRow();
+		$this->update('element_type',array('display_order'=>10),"event_type_id = {$event_type['id']} and class_name = 'Element_OphCiOperationdayassessment_Anaesthetic'");
+		$this->update('element_type',array('display_order'=>20),"event_type_id = {$event_type['id']} and class_name = 'OEElementDayOfOperation'");
 	}
 
 	public function down()
 	{
-		$event_type = EventType::model()->find('class_name=?',array('OphCiOperationdayassessment'));
-		$this->update('element_type',array('display_order'=>10),"event_type_id = $event_type->id and class_name = 'OEElementDayOfOperation'");
-		$this->update('element_type',array('display_order'=>20),"event_type_id = $event_type->id and class_name = 'Element_OphCiOperationdayassessment_Anaesthetic'");
+		$event_type = $this->dbConnection->createCommand()->select("*")->from("event_type")->where("class_name = :class_name",array(":class_name" => "OphCiOperationdayassessment"))->queryRow();
+		$this->update('element_type',array('display_order'=>10),"event_type_id = {$event_type['id']} and class_name = 'OEElementDayOfOperation'");
+		$this->update('element_type',array('display_order'=>20),"event_type_id = {$event_type['id']} and class_name = 'Element_OphCiOperationdayassessment_Anaesthetic'");
 	}
 }
